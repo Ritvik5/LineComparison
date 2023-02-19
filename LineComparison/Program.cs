f3f7f38bd74459e144b76f3a58312de3d11bb18f
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-19 13:53:32 +0530

    Length Between Two Point

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Program");
+            //Constants
+            int x1 = 1;
+            int y1 = 2;
+            int x2 = 3;
+            int y2 = 4;
+            //Variables
+            double dist = 0;
+            dist = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine("Distance Between Two Point is " + dist);
         }
     }
 }
