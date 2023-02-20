26f0944b44834b09475e32112f312d22a1beaa1e
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-20 17:57:37 +0530

    Comparing Two Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,19 +4,35 @@
     {
         static void Main(string[] args)
         {
-            double a1 = 1;
-            double a2 = 2;
-            double b1 = 3;
-            double b2 = 6;
-            double c1 = 4;
-            double c2 = 8;
+            //Coordinates of the First Line
+            double x1 = 0;
+            double y1 = 0;
+            double x2 = 5;
+            double y2 = 5;
 
-            if ((a1 / a2 == b1 / b2) && (c1 / c2 == a1 / a2) && (b1 / b2 == c1 / c2))
+            //Coordinates of the Second line
+            double x3 = 0;
+            double y3 = 0;
+            double x4 = 10;
+            double y4 = 10;
+
+            //Calculating the lenghts of the two lines
+            double lenght1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double lenght2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+
+            if (lenght1 == lenght2)
             {
-                Console.WriteLine("The Two lines are Equal");
+                Console.WriteLine("The Two Lines Are Equal");
+            }
+            else if (lenght1 > lenght2)
+            {
+                Console.WriteLine("The First Line is greter than Second line");
             }
             else
-                Console.WriteLine("The Two Lines are not Equal");
+            {
+                Console.WriteLine("The Second Line is Greater than the first line");
+            }
+            
         }
     }
 }
